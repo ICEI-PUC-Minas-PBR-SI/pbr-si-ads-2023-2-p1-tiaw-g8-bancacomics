2418e6aed24f5c121cb83dd967558866608238b8
Marcos Gomes <marcosvinicius08000@gmail.com>
2023-11-03 15:42:56 -0300

    Pagina detalhes

--- src/Repository/HeroiRepository.js
+++ src/Repository/HeroiRepository.js
@@ -1,6 +1,6 @@
 class HeroiInterface {
     constructor() {
-        
+
     }
 
     getAllHeros() {
@@ -9,9 +9,9 @@ class HeroiInterface {
         const apiUrl = 'https://gateway.marvel.com/v1/public/characters';
         const ts = new Date().getTime();
         const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
-    
+
         const url = `${apiUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-    
+
         return fetch(url)
             .then((response) => {
                 if (!response.ok) {
@@ -23,5 +23,26 @@ class HeroiInterface {
                 return data.data.results; // Ajuste de acordo com a estrutura dos dados da API
             });
     }
-    
+
+    getHeroById(id) {
+        try {
+            const publicKey = 'c21a8082ad09f56a486b54525e571e4b';
+            const privateKey = 'e83ebfb95b8eba16ea4755b8c17b0a7fa8772db5';
+            const apiUrl = `https://gateway.marvel.com/v1/public/characters/${id}`;
+            const ts = new Date().getTime();
+            const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+            const url = `${apiUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+            return fetch(url).then((response) =>{
+                if(!response.ok){
+                    throw new Error('Erro ao encontrar heroi')
+                }
+                return response.json();
+            }).then((data) => {
+                return data.data.results;
+            })
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
 }
