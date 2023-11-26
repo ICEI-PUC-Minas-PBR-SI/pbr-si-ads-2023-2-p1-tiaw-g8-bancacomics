5f7799807a2c7bd58494bcbf4c5f898670e1ca3a
Marcos Gomes <marcosvinicius08000@gmail.com>
2023-11-26 17:55:17 -0300

    novas funcionalidades

--- src/Repository/HeroiRepository.js
+++ src/Repository/HeroiRepository.js
@@ -9,7 +9,6 @@ class HeroiInterface {
         const apiUrl = 'https://gateway.marvel.com/v1/public/characters';
         const ts = new Date().getTime();
         const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
-
         const url = `${apiUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
 
         return fetch(url)
@@ -43,6 +42,22 @@ class HeroiInterface {
         } catch(error) {
             console.error(error);
         }
+    }
+
+    getHeroByUrl(apiUrl){
+        const publicKey = 'c21a8082ad09f56a486b54525e571e4b';
+        const privateKey = 'e83ebfb95b8eba16ea4755b8c17b0a7fa8772db5';
+        const ts = new Date().getTime();
+        const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+        const url = `${apiUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+        return fetch(url).then((response) =>{
+            if(!response.ok){
+                throw new Error('Erro ao encontrar o heroi pel url')
+            }
+            return response.json();
+        }).then((hero) =>{
+            return hero.data.results;
+        })
     }
 
 }
